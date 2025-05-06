5cc6161082ed2f1eaa4b3ac2ecc6d2101fb892ee
ChloeD-L <linduan1018@gmail.com>
2025-05-06 14:52:40 +1000

    Revert db setup in test & add model object in activity

--- test/setup/db.js
+++ test/setup/db.js
@@ -1,9 +1,4 @@
 import UserBuilder from '../__tests__/builders/userBuilder';
-import TypeBuilder from '../__tests__/builders/typeBuilder';
-import StatusBuilder from '../__tests__/builders/statusBuilder';
-import LabelBuilder from '../__tests__/builders/labelBuilder';
-import EpicBuilder from '../__tests__/builders/epicBuilder';
-import SprintBuilder from '../__tests__/builders/sprintBuilder';
 import dbHandler from './dbHandler';
 
 let dbConnection = null;
@@ -11,8 +6,6 @@ let tenantsConnection = null;
 let isInitialized = false;
 let tenantId = '62e333606fb0da0a12dcfe78';
 let defaultUser = null;
-let defaultTicketType = null;
-let defaultStatus = null;
 
 async function connect() {
   if (!isInitialized) {
@@ -33,14 +26,6 @@ async function connect() {
 async function createDefaultData() {
   const user = await new UserBuilder().save();
   defaultUser = user;
-  const types = await TypeBuilder.createDefaultTypes();
-  const statuses = await StatusBuilder.createDefaultStatuses();
-  const label = await new LabelBuilder().save();
-  const epic = await new EpicBuilder().save();
-  const sprint = await new SprintBuilder().save();
-
-  defaultTicketType = types[0];
-  defaultStatus = statuses[0];
 }
 
 async function clearDatabase() {
@@ -58,12 +43,6 @@ export default {
   },
   get defaultUser() {
     return defaultUser;
-  },
-  get defaultTicketType() {
-    return defaultTicketType;
-  },
-  get defaultStatus() {
-    return defaultStatus;
   },
   tenantId,
 };
