26037d2c776a40d3db1a9f929c051d0f1693dc0d
Dollar0712 <1416243855@qq.com>
2025-05-13 14:45:50 +1000

    Removed defaut type creation from getTicketType() & Refined type test

--- test/__tests__/type.test.js
+++ test/__tests__/type.test.js
@@ -2,34 +2,60 @@ import request from 'supertest';
 import httpStatus from 'http-status';
 import app from '../setup/app';
 import axios from 'axios';
+import TypeBuilder from './builders/typeBuilder';
 
 describe('Types Test', () => {
   it('should get 4 types', async () => {
+    const expectedTypeData = [
+      {
+        name: 'Story',
+        slug: 'story',
+        icon: 'https://010001.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10315?size=medium',
+      },
+      {
+        name: 'Task',
+        slug: 'task',
+        icon: 'https://010001.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10318?size=medium',
+      },
+      {
+        name: 'Bug',
+        slug: 'bug',
+        icon: 'https://010001.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10303?size=medium',
+      },
+      {
+        name: 'Tech Debt',
+        slug: 'techDebt',
+        icon: 'https://010001.atlassian.net/rest/api/2/universal_avatar/view/type/issuetype/avatar/10308?size=medium',
+      },
+    ];
+    // Create default types
+    const expectedTypes = await Promise.all(
+      expectedTypeData.map((typeData) => {
+        return new TypeBuilder()
+          .withName(typeData.name)
+          .withSlug(typeData.slug)
+          .withIcon(typeData.icon)
+          .save();
+      })
+    );
+
     const res = await request(app.application)
       .get('/api/v2/types')
       .expect(httpStatus.OK);
     
       expect(res.body).toHaveLength(4);
 
-      const expectedTypeNames = ['Story', 'Bug', 'Task', 'Tech Debt'];
-      const expectedTypeSlugs = ['story', 'bug', 'task', 'techDebt'];
-
       const actualTypeNames = res.body.map((type) => type.name);
       const actualTypeSlugs = res.body.map((type) => type.slug);
+      const actualTypeIcons = res.body.map((type) => type.icon);
 
-      expectedTypeNames.forEach((typeName) => {
-        expect(actualTypeNames).toContain(typeName);
-      });
-
-      expectedTypeSlugs.forEach((typeSlug) => {
-        expect(actualTypeSlugs).toContain(typeSlug);
-      });
-
-      for (const type of res.body) {
-        expect(type).toHaveProperty('icon');
+      expectedTypes.forEach(async (type) => {
+        expect(actualTypeNames).toContain(type.name);
+        expect(actualTypeSlugs).toContain(type.slug);
+        expect(actualTypeIcons).toContain(type.icon);
 
         const response = await axios.get(type.icon);
         expect(response.status).toBe(httpStatus.OK);
-      }
+      });
   });
 });
