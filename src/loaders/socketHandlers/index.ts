7634ec62540269c836d95e66d2eab966d159daff
Dollar0712 <1416243855@qq.com>
2025-06-07 20:32:59 +1000

    Removed debug logging

--- src/loaders/socketHandlers/index.ts
+++ src/loaders/socketHandlers/index.ts
@@ -3,13 +3,9 @@ import { registerRetroSprintRoomHandler } from './eventHandler';
 
 export default function registerSocketHandlers(io: Server) {
   io.on('connection', (socket: Socket) => {
-    // eslint-disable-next-line no-console
-    console.log(`Socket connected: ${socket.id}`);
     registerRetroSprintRoomHandler(io, socket);
     
     socket.on('disconnect', () => {
-      // eslint-disable-next-line no-console
-      console.log(`Socket disconnected: ${socket.id}`);
     });
   });
 }
