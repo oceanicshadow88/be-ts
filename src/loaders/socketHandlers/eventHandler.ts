0f416f6174f990182e00259a19b23acc40dd38e9
Dollar0712 <1416243855@qq.com>
2025-06-07 20:59:41 +1000

    Removed debug msg

--- src/loaders/socketHandlers/eventHandler.ts
+++ src/loaders/socketHandlers/eventHandler.ts
@@ -4,8 +4,6 @@ import { Server, Socket } from 'socket.io';
 function registerRetroSprintRoomHandler(io: Server, socket: Socket) {
   socket.on('join_retro_sprint_room', (sprintId: string) => {
     socket.join(`retro_sprint_room_${sprintId}`);
-    // eslint-disable-next-line no-console
-    console.log(`Socket ${socket.id} joined retro sprint room: ${sprintId}`);
   });
 }
 
