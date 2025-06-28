73c746f2403d251da431ab6245bdb60c0518bf9a
Dollar0712 <1416243855@qq.com>
2025-06-28 19:05:51 +1000

    Added test suite for login & Added related builder

--- test/__tests__/register.test.js
+++ test/__tests__/register.test.js
@@ -1,45 +1,11 @@
 import request from 'supertest';
-import { invalidForm, validForm } from '../fixtures/register';
-import dbHandler from '../dbHandler';
+import app from '../setup/app';
+import db from '../setup/db';
+import UserBuilder from './builders/userBuilder';
 
-
-let application = null;
-// let dbConnection = '';
-
-beforeAll(async () => {
-//   let result = await dbHandler.connect();
-//   dbConnection = result.mainConnection;
-//   tenantConnection = result.tenantConnection;
-
-  async function loadApp() {
-    const appModule = await import('../../src/loaders/express');
-    const app = appModule.default;
-    application = app();
-  }
-  await loadApp();
-});
-
-afterAll(async () => {
-  await dbHandler.closeDatabase();
-});
-
-
-describe('REgister', () => {
-  it('should success if user has correct data', async () => {
+describe('Register API tests', () => {
+  it('should register a company if correct data provided', async () => {
     const res = await request(application).post('/api/v2/register').send(validForm);
     expect(res.statusCode).toEqual(200);
   });
-  //   it('should return 400 if already have company', async () => {
-  //     const res = await request(application).post('/api/v2/register').send(validForm);
-  //     expect(res.statusCode).toEqual(202);
-  //   });
-  //   it('should return 400 have invalid company name', async () => {
-  //     const res = await request(application).post('/api/v2/register').send(validForm);
-  //     expect(res.statusCode).toEqual(202);
-  //   });
-
-//   it('should return 500 for any other errors', async () => {
-//     const res = await request(application).post('/api/v2/register').send(validForm);
-//     expect(res.statusCode).toEqual(202);
-//   });
 });
