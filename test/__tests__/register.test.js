91f7c3afaf6b8396581015cf1b73aee13cbe9a65
Dollar0712 <1416243855@qq.com>
2025-06-28 21:49:36 +1000

    Added tests for uppercased email

--- test/__tests__/register.test.js
+++ test/__tests__/register.test.js
@@ -19,4 +19,21 @@ describe('Register API tests', () => {
     expect(res.body.data.newUser.email).toBe(db.defaultUser.email);
     expect(res.body.data.newUser.id).toBe(db.defaultUser.id);
   });
+
+  it('should register a company for uppercased email', async () => {
+    const company = 'testcompany';
+  
+    const res = await request(app.application)
+      .post('/api/v2/register')
+      .send({
+        company: company,
+        email: db.defaultUser.email.toUpperCase(),
+      });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.data.newTenants.origin).toBe(`${config.protocol}${company}.${config.mainDomain}`);
+    expect(res.body.data.newTenants.owner).toBe(db.defaultUser.id);
+    expect(res.body.data.newUser.email).toBe(db.defaultUser.email.toLowerCase());
+    expect(res.body.data.newUser.id).toBe(db.defaultUser.id);
+  });
 });
